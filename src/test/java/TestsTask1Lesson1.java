183e14160247ca95b9fe5d5a1324904ee67fba2e
Mikhailo Petrenko <petrenko.mikhailo@pdffiller.team>
2021-02-15 20:47:10 +0200

    Second commit

--- src/test/java/TestsTask1Lesson1.java
+++ src/test/java/TestsTask1Lesson1.java
@@ -1,13 +1,16 @@
 package test.java;
 
 import main.java.Server;
+import main.java.lesson1.Task1;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class TestsTask1Lesson1 {
     @Test
-    public void PositiveTest(){
+    public void FirstNumberIsMinimumPositive(){
+        Task1 task1 = new Task1();
+
 
     }
 }
