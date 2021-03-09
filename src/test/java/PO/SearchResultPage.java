a977ed011c7416268b89a7c117a02acc730152d5
Mikhailo Petrenko <petrenko.mikhailo@pdffiller.team>
2021-03-09 14:12:49 +0200

    First commit add test for test Brand

--- src/test/java/PO/SearchResultPage.java
+++ src/test/java/PO/SearchResultPage.java
@@ -11,6 +11,19 @@ import java.util.List;
 public class SearchResultPage extends BasePage {
 
     private final By getResultLocator = By.cssSelector("span[class='a-size-medium a-color-base a-text-normal']");
+    //private final By checkBoxBrandLocator = By.xpath("//ul[@aria-labelledby='p_89-title']//span[@class='a-label a-checkbox-label']");
+    private final By SeeMoreBrandLocator = By.cssSelector("a[aria-label='See more, Brand']");
+
+//    private final By checkBoxHPBrandLocator = By.xpath("//li[@id= 'p_89/HP']//span");
+//    private final By checkBoxAcerBrandLocator = By.xpath("//li[@id= 'p_89/Acer']//span");
+//    private final By checkBoxLenovoBrandLocator = By.xpath("//li[@id= 'p_89/Lenovo']//span");
+//    private final By checkBoxAsusBrandLocator = By.xpath("//li[@id= 'p_89/ASUS']//span");
+//    private final By checkBoxDellBrandLocator = By.xpath("//li[@id= 'p_89/Dell']//span");
+//    private final By checkBoxMicrosoftBrandLocator = By.xpath("//li[@id= 'p_89/Microsoft']//span");
+//    private final By checkBoxAppleBrandLocator = By.xpath("//li[@id= 'p_89/Apple']//span");
+//    private final By checkBoxCHUWIBrandLocator = By.xpath("//li[@id= 'p_89/CHUWI']//span");
+//    private final By checkBoxSamsungBrandLocator = By.xpath("//li[@id= 'p_89/Samsung Electronics']//span");
+//    private final By checkBoxMSIBrandLocator = By.xpath("//li[@id= 'p_89/MSI']//span");
 
     public SearchResultPage(WebDriver driver) {
         super(driver);
@@ -22,4 +35,20 @@ public class SearchResultPage extends BasePage {
         List<WebElement> searchBlock = driver.findElements(getResultLocator);
         return searchBlock;
     }
+
+//    public void chooseCheckBoxBrand(){
+//        wait.until(ExpectedConditions.elementToBeClickable(checkBoxBrandLocator));
+//        driver.findElement(checkBoxBrandLocator).click();
+//    }
+
+    public void chooseCheckBoxBrand(By elementLocatore){
+        wait.until(ExpectedConditions.elementToBeClickable(elementLocatore));
+        driver.findElement(elementLocatore).click();
+    }
+
+    public void tickSeeMoreBrand(){
+        wait.until(ExpectedConditions.elementToBeClickable(SeeMoreBrandLocator));
+        driver.findElement(SeeMoreBrandLocator).click();
+    }
+
 }
---
+++ src/test/java/PageObjectHomeWork/CheckFilter.java
@@ -0,0 +1,65 @@
+package test.java.PageObjectHomeWork;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import test.java.PO.HomePage;
+import test.java.PO.SearchResultPage;
+import test.java.TestBaseSetup;
+
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class CheckFilter extends TestBaseSetup {
+    String laptop = "laptop";
+    //String brand = "HP";
+    HomePage homePage;
+    SearchResultPage searchResultPage;
+
+    @BeforeClass
+    public void beforeClass(){
+
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        homePage = new HomePage(driver);
+        homePage.openPage();
+        searchResultPage = new SearchResultPage(driver);
+    }
+
+    @Test (dataProvider = "DProvider")
+    public void checkLaptopBrand(String brand, By locator) {
+        homePage.clickSearchField();
+        homePage.fillDataToSearchField(laptop);
+        homePage.clickSearchButton();
+
+        searchResultPage.tickSeeMoreBrand();
+        searchResultPage.chooseCheckBoxBrand(locator);
+
+        for(WebElement res: searchResultPage.getElementList()){
+            assertTrue(res.getText().toLowerCase().contains(brand.toLowerCase()));
+        }
+
+    }
+
+    @DataProvider
+    public  Object [][] DProvider(){
+        return new Object[][]{
+                {"HP",By.xpath("//li[@id= 'p_89/HP']//span")},
+                {"Acer", By.xpath("//li[@id= 'p_89/Acer']//span")},
+                {"Lenovo", By.xpath("//li[@id= 'p_89/Lenovo']//span")},
+                {"ASUS", By.xpath("//li[@id= 'p_89/ASUS']//span")},
+                {"Dell" , By.xpath("//li[@id= 'p_89/Dell']//span")},
+                {"Microsoft", By.xpath("//li[@id= 'p_89/Microsoft']//span")},
+                {"Apple", By.xpath("//li[@id= 'p_89/Apple']//span")},
+                {"CHUWI", By.xpath("//li[@id= 'p_89/CHUWI']//span")},
+                {"Samsung", By.xpath("//li[@id= 'p_89/Samsung Electronics']//span")},
+                {"MSI", By.xpath("//li[@id= 'p_89/MSI']//span")}
+        };
+    }
+}
